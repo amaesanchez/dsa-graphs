0076a41dab83f30101a654f8a49319ba31b0e183
Arlaine Sanchez <amaesanchez@gmail.com>
2023-02-09 15:32:20 -0500

    up to dfs

--- graph.js
+++ graph.js
@@ -16,26 +16,63 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      if (!this.nodes.has(vertex)) {
+        this.nodes.add(vertex)
+      }
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    if (v1.adjacent.has(v2)) {
+      v1.adjacent.delete(v2);
+      v2.adjacent.delete(v1);
+    }
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+    const neighbors = vertex.adjacent
+
+    for (let node of neighbors) {
+      node.adjacent.delete(vertex);
+      vertex.adjacent.delete(node);
+    }
+
+    this.nodes.delete(vertex);
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start, seen = new Set(start.value)) {
+
+    for (let neighbor of start.adjacent) {
+      console.log("neighbor", neighbor)
+      if (!seen.has(neighbor)) {
+        seen.add(neighbor.value);
+        seen = this.depthFirstSearch(neighbor, seen);
+      }
+    }
+
+    console.log("seen", seen)
+    return Array.from(seen);
+  }
 
   /** traverse graph with BDS and returns array of Node values */
   breadthFirstSearch(start) { }
